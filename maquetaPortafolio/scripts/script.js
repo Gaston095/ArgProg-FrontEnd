bbc0d389945d3c29c57b1b8c387913a1aeaee343
Gaston Salas <56091876+Gaston095@users.noreply.github.com>
2022-12-23 13:50:58 -0300

    Delete maquetaPortafolio directory

--- maquetaPortafolio/scripts/script.js
+++
@@ -1,7 +0,0 @@
-let menuItem = querySelector('#nav__menu-item');
-let btnMenu = querySelector('#nav__btnMenu');
-
-btnMenu.addEventListener('click', function() {
-	menuItem.classList.toggle('menuToggle');
-})
-
