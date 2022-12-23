1ae9133437a459941d76fcbb151912b6c8a447b1
Gaston095 <Gastonsalas095@gmail.com>
2022-12-23 13:46:45 -0300

    Se organizo el proyecto en carpetas de Frontend y Backend

---
+++ BackEnd/gaston/src/main/java/com/portfolio/gaston/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.gaston.Repository;
+
+import com.portfolio.gaston.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}
---
+++ BackEnd/gaston/src/main/java/com/portfolio/gaston/Service/ImpPersonaService.java
@@ -0,0 +1,37 @@
+
+package com.portfolio.gaston.Service;
+
+import com.portfolio.gaston.Entity.Persona;
+import com.portfolio.gaston.Interface.IPersonaService;
+import com.portfolio.gaston.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService {
+    @Autowired IPersonaRepository ipersonaRepository;
+    
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
